d448116ec88ef5bddc36ef204e64a66b1e7b2146
alextakahashi <alextakahashi@berkeley.edu>
2016-10-23 21:52:22 -0700

    Shows TableView w/CollectionView

---
+++ berkeleyMobileiOS/HomeViewController.swift
@@ -0,0 +1,60 @@
+//
+//  HomeViewController.swift
+//  berkeleyMobileiOS
+//
+//  Created by Alex Takahashi on 10/23/16.
+//  Copyright © 2016 org.berkeleyMobile. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    
+    // HARD CODE
+    let cellNames = ["Gyms", "Dining"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return cellNames[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = homeTableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell")! as! HomeTableViewCell
+        if let layout = cell.homeCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+        cell.homeCollectionView.delegate = cell
+        cell.homeCollectionView.dataSource = cell
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
