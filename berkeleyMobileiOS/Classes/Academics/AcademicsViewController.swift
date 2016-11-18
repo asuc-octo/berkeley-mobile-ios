d585a71951ecfc4e155862321838e12e15aa924b
Akilesh Bapu <akilesh@berkeley.edu>
2016-11-17 21:56:22 -0800

    Added Navigation Controllers

--- berkeleyMobileiOS/Classes/Academics/AcademicsViewController.swift
+++ berkeleyMobileiOS/Classes/Academics/AcademicsViewController.swift
@@ -10,18 +10,9 @@ import UIKit
 import Material
 class AcademicsViewController: BaseViewController {
     //Sets up initial tab look for this class
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        preparePageTabBarItem()
-    }
     override func viewDidLoad() {
         self.sectionNames = ["Doe", "Main Stacks"]
-        self.baseTitleLabel.text = "Academics"
         self.baseTableView.reloadData()
-    }
-    //Make sure tab bar is highlighted properly
-    override func viewDidAppear(_ animated: Bool) {
-        ConvenienceMethods.setCurrentTabStyle(pageTabBarVC: pageTabBarController!, ForSelectedViewController: self)
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = baseTableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell")! as! HomeTableViewCell
@@ -32,12 +23,6 @@ class AcademicsViewController: BaseViewController {
         cell.homeCollectionView.delegate = cell
         cell.homeCollectionView.dataSource = cell
         return cell
-    }
-    //Customize Tab Bar Presence
-    private func preparePageTabBarItem() {
-        pageTabBarItem.image = #imageLiteral(resourceName: "library")
-        pageTabBarItem.image = pageTabBarItem.image!.withRenderingMode(.alwaysTemplate)
-        pageTabBarItem.imageView?.contentMode = .scaleAspectFit
     }
 
 }
