1406b2f8af7a1fdf3955b8e3b4666299c5e67d80
alextakahashi <alextakahashi@berkeley.edu>
2016-11-20 18:46:18 -0800

    Problems w/tableview

--- berkeleyMobileiOS/Classes/Recreation/RecreationViewController.swift
+++ berkeleyMobileiOS/Classes/Recreation/RecreationViewController.swift
@@ -9,16 +9,19 @@
 import UIKit
 import Material
 class RecreationViewController: BaseViewController {
+    
+    let recDict = 
+    
     //Sets up initial tab look for this class
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         preparePageTabBarItem()
     }
     override func viewDidLoad() {
-        self.sectionNames = ["RSF", "Memorial Stadium"]
+        self.sectionNames = ["Facilities", "Courts", "Aquatics", "Fields", "Group Classes"]
         self.baseTableView.reloadData()
-
     }
+    
     //Make sure tab bar is highlighted properly
     override func viewDidAppear(_ animated: Bool) {
         ConvenienceMethods.setCurrentTabStyle(pageTabBarVC: pageTabBarController!, ForSelectedViewController: self)
@@ -26,14 +29,28 @@ class RecreationViewController: BaseViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = baseTableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell")! as! HomeTableViewCell
-        cell.collectionCellNames = ["Doge", "Doggo", "Yapper"]
+        if indexPath.section == 0 {
+            cell.collectionCellNames = ["RSF", "Stadium", "Edwards Track"]
+        } else if indexPath.section == 1 {
+            cell.collectionCellNames = ["Field House", "RSF Gold", "RSF Blue", "Golden Bear", "Hearst"]
+        } else if indexPath.section == 2 {
+            cell.collectionCellNames = ["Gold Bear", "Hearst", "Spieker", "Strawberry Canyon"]
+        } else if indexPath.section == 3 {
+            cell.collectionCellNames = ["Underhill", "Maxwell"]
+        } else {
+            cell.collectionCellNames = ["All Around", "Aqua", "Cardio", "Core", "Dance", "Mind/Body", "Strength"]
+        }
+        print(indexPath.section)
         if let layout = cell.homeCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .horizontal
         }
         cell.homeCollectionView.delegate = cell
         cell.homeCollectionView.dataSource = cell
         return cell
+        
     }
+
+    
     //Customize Tab Bar Presence
     private func preparePageTabBarItem() {
         pageTabBarItem.image = #imageLiteral(resourceName: "50x50-Gym_32x32")
