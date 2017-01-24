c414a3c9405fd5fe2aa5f69c12742c07b7948bc1
Bohui Moon <bohuimoon@gmail.com>
2017-01-24 13:15:54 -0800

    Refactored DiningHall & Menu VC scroll interactions.
    - New prototype DelegatesScroll abstraction their connections.
    - The scroll shadow control moved to DiningMenuVC
    - Scroll shadow is now applied to the open hours label (tentative)

--- berkeleyMobileiOS/Classes/Dining/DiningMenu/DiningMenuViewController.swift
+++ berkeleyMobileiOS/Classes/Dining/DiningMenu/DiningMenuViewController.swift
@@ -6,11 +6,10 @@ fileprivate let kColorNavy = UIColor(red: 23/255.0, green: 85/255.0, blue: 122/2
 /**
  * ViewController to display list of menus of one MealType at a certain DiningHall.
  */
-class DiningMenuViewController: UIViewController, RequiresData, UITableViewDataSource, UITableViewDelegate
+class DiningMenuViewController: UIViewController, RequiresData, DelegatesScroll, UITableViewDataSource, UITableViewDelegate
 {
     // Data
     private var shift: MealShift!
-    
     
     //UI
     @IBOutlet private weak var hoursView: UIView!
@@ -29,26 +28,41 @@ class DiningMenuViewController: UIViewController, RequiresData, UITableViewDataS
         self.shift = data.shift
         
         self.pageTabBarItem.title = data.type.name
-        self.pageTabBarItem.titleColor = kColorNavy
-        self.pageTabBarItem.pulseColor = kColorNavy//UIColor.white
     }
     
     
     // ========================================
     // MARK: - UIViewController
     // ========================================
-    /**
-     * 
-     */
+    
+    /// Configure the pageTabItem. 
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        
+        self.pageTabBarItem.titleColor = kColorNavy
+        self.pageTabBarItem.pulseColor = kColorNavy//UIColor.white
+    }
+    
+    /// Configure the tableView and open hours label. 
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        self.view!.clipsToBounds = true
+        self.view!.sendSubview(toBack: self.tableView)
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.isScrollEnabled = false
         
         // Display the open hours.
+        let hoursLayer = self.hoursView.layer
+        hoursLayer.shadowRadius = 2
+        hoursLayer.shadowOpacity = 0.2
+        hoursLayer.shadowColor = UIColor.clear.cgColor
+        hoursLayer.shadowOffset = CGSize(width: 0, height: 2)
+        
         if self.shift.open.isNil || self.shift.close.isNil {
             self.hoursLabel.text = "CLOSED"
             return
@@ -80,5 +94,55 @@ class DiningMenuViewController: UIViewController, RequiresData, UITableViewDataS
         cell.setData( self.shift.menu[indexPath.row] )
         
         return cell
+    }
+    
+    
+    // ========================================
+    // MARK: - DelegatesScroll
+    // ========================================
+    
+    /// Return the current size of the tableView.
+    var contentSize: CGSize
+    {
+        get { return self.tableView.contentSize }
+    }
+    
+    /// Return the current offset or pass the set offset action.
+    var contentOffset: CGPoint
+    {
+        get { return self.tableView.contentOffset }
+        set { self.tableView.contentOffset = newValue }
+    }
+    
+    /// Relay the set action.
+    func setContentOffset(_ offset: CGPoint, animated: Bool)
+    {
+        self.tableView.setContentOffset(offset, animated: animated)
+    }
+    
+    
+    // ========================================
+    // MARK: - UIScrollViewDelegate
+    // ========================================
+    
+    /// When scrolled down, apply shadow to hoursView.
+    func scrollViewDidScroll(_ scrollView: UIScrollView)
+    {
+        let offset = scrollView.contentOffset.y
+        self.showScrollShadow = offset > 0
+    }
+    
+    
+    // ========================================
+    // MARK: - Private
+    // ========================================
+    
+    /// Sets MenuTabBar's shadow to black to show, and clear to hide.
+    private var showScrollShadow: Bool = false
+    {
+        didSet 
+        {
+            self.hoursView.layer.shadowColor = (showScrollShadow ? UIColor.black : UIColor.clear).cgColor
+        }
     }
 }
