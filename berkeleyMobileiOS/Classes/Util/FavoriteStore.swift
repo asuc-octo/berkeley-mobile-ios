ca87fc61fb4d7ee1ac1a83b0f14aad54db61e73e
Bohui Moon <bohuimoon@gmail.com>
2017-01-27 14:04:21 -0800

    Updated FavoriteStore with `contains` func

--- berkeleyMobileiOS/Classes/Util/FavoriteStore.swift
+++ berkeleyMobileiOS/Classes/Util/FavoriteStore.swift
@@ -2,6 +2,17 @@
 import Foundation
 import RealmSwift
 
+
+/// Global, but private, Realm.
+fileprivate let realm = try! Realm()
+
+/// Represents a single favorited item (or location).
+class FavoriteItem: RealmSwift.Object
+{
+    dynamic var type: String = ""
+    dynamic var name: String = ""
+}
+
 /**
  * FavoriteStore is a singleton that keeps track of favorited items for this device, 
  * regardless of the type (e.g. Dining Hall, Gym, Library, etc).
@@ -16,6 +27,7 @@ class FavoriteStore
     static let sharedInstance = FavoriteStore()
     private init() {}
     
+    /// Add item of given type and name.
     func add(type: AnyClass, name: String)
     {
         let item = FavoriteItem(value: ["type": String(describing: type), "name": name])
@@ -26,9 +38,10 @@ class FavoriteStore
         }
     }
     
+    /// Remove item of given type and name.
     func remove(type: AnyClass, name: String)
     {
-        let result = realm.objects(FavoriteItem.self).filter("type = '\( String(describing: type) )' and name = '\(name)'")
+        let result = self.query(type, name)
         
         if result.count == 1
         {
@@ -38,21 +51,24 @@ class FavoriteStore
         }
     }
     
+    /// Returns whether item of given type and name is favorited.
+    func contains(type: AnyClass, name: String) -> Bool
+    {
+        let result = self.query(type, name)
+        
+        return result.count == 1
+    }
+    
     /// Return a String array containing all favorited items of the specified class. 
     func allItemsOfType(_ type: AnyClass) -> [String]
     {
         let result = realm.objects(FavoriteItem.self).filter("type = '\( String(describing: type) )'")
         return result.map { return $0.name }
     }
-}
-
-
-/// Global, but private, Realm.
-fileprivate let realm = try! Realm()
-
-/// Represents a single favorited item (or location).
-class FavoriteItem: RealmSwift.Object
-{
-    dynamic var type: String = ""
-    dynamic var name: String = ""
+    
+    
+    private func query(_ type: AnyClass, _ name: String) -> Results<FavoriteItem>
+    {
+        return realm.objects(FavoriteItem.self).filter("type = '\( String(describing: type) )' and name = '\(name)'")
+    }
 }
