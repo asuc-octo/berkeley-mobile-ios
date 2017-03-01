efb796b07434e4c9880097989e30e4c949b16711
dudduss <duddus@berkeley.edu>
2017-02-28 23:25:38 -0800

    nice headers

--- berkeleyMobileiOS/Classes/Recreation/GymClassViewController.swift
+++ berkeleyMobileiOS/Classes/Recreation/GymClassViewController.swift
@@ -27,6 +27,8 @@ class GymClassViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.gray
+        
         gymClassesTableView.dataSource = self
         gymClassesTableView.delegate = self
         
@@ -157,34 +159,80 @@ class GymClassViewController: UIViewController, UITableViewDelegate, UITableView
         formatter2.dateStyle = .full
         let exactDate = Date(timeInterval: 24*60*60*(Double(section)), since: currentDate!)
         
-        let r = formatter2.string(from: exactDate)
+        var r = formatter2.string(from: exactDate)
+        let endIndex = r.index(r.endIndex, offsetBy: -6)
+        r = r.substring(to: endIndex)
 
         return r
     }
     
+//    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int)
+//    {
+//        let header = view as! UITableViewHeaderFooterView
+//        header.textLabel?.textColor = UIColor.white
+//        let vw = UIView()
+//        vw.backgroundColor = UIColor.blue
+//        header.backgroundView = vw
+//        header.backgroundColor = UIColor.blue
+//    }
+
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView = Bundle.main.loadNibNamed("GymClassTableHeaderView", owner: self, options: nil)?.first as! GymClassTableHeaderView
+        
+        
+        let myFormatter = DateFormatter()
+        myFormatter.dateStyle = .short
+        let s = myFormatter.string(from: Date())
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "PST")
+        let currentDate = dateFormatter.date(from: s)
+        
+        
+        let formatter2 = DateFormatter()
+        formatter2.dateStyle = .full
+        let exactDate = Date(timeInterval: 24*60*60*(Double(section)), since: currentDate!)
+        
+        var r = formatter2.string(from: exactDate)
+        let endIndex = r.index(r.endIndex, offsetBy: -6)
+        r = r.substring(to: endIndex)
+        
+        headerView.headerDate.text = r
+        
+        var rArr = r.components(separatedBy: ",")
+        var day = rArr[0]
+        var abbrev = ""
+        let kindex = day.index(day.startIndex, offsetBy: 0)
+        let lindex = day.index(day.startIndex, offsetBy: 2)
+        if (day[kindex] == "T") || (day[kindex] == "S") {
+            abbrev = String(day[Range(kindex ..< lindex)])
+        } else {
+            abbrev = String(day[kindex])
+        }
+        
+        headerView.headerAbbreviation.text = abbrev
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        if (gymClasses[section].count == 0) {
+            return 0
+        }
+        
+        return 60
+    }
+//
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "gymClass") as! GymClassCell
         
         var gymClass:GymClass?
         
         gymClass = gymClasses[indexPath.section][indexPath.row]
-        
-//        if (indexPath.section == 0) {
-//            gymClass = todayClasses[indexPath.row]
-//        } else if (indexPath.section == 1) {
-//            gymClass = oneDayAheadClasses[indexPath.row]
-//        } else if (indexPath.section == 2) {
-//            gymClass = twoDaysAheadClasses[indexPath.row]
-//        } else if (indexPath.section == 3) {
-//           gymClass = threeDaysAheadClasses[indexPath.row]
-//        } else if (indexPath.section == 4) {
-//           gymClass = fourDaysAheadClasses[indexPath.row]
-//        } else if (indexPath.section == 5) {
-//            gymClass = fiveDaysAheadClasses[indexPath.row]
-//        } else {
-//            gymClass = sixDaysAheadClasses[indexPath.row]
-//        }
-//    
         
         cell.name.text = gymClass!.name
         cell.instructor.text = "Instructor: " + gymClass!.trainer!
@@ -204,6 +252,7 @@ class GymClassViewController: UIViewController, UITableViewDelegate, UITableView
     
         return cell
     }
+    
 
 
 }
