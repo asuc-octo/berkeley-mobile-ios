2bf70287a6f3639ff6df29a40d893e3ebf6f6241
Akilesh Bapu <akilesh@berkeley.edu>
2017-03-30 01:11:45 -0700

    BearTransit Worked in

--- berkeleyMobileiOS/AppDelegate.swift
+++ berkeleyMobileiOS/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyAsOV94_am2S5ceDbfwkMDelYGCx6AOPgU")
+        GMSPlacesClient.provideAPIKey("AIzaSyAssh2z-Owx9fTzth__xnfEp-h2dG120Fk")
+
         return true
     }
 
