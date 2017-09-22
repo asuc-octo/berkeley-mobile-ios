12145f48477e1b87e94b7b32eaa5cc9a96f584f9
akbapu14 <akilesh@berkeley.edu>
2017-09-21 18:46:12 -0700

    Google Place Autocomplete fixed

--- berkeleyMobileiOS/AppDelegate.swift
+++ berkeleyMobileiOS/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyAsOV94_am2S5ceDbfwkMDelYGCx6AOPgU")
+        GMSServices.provideAPIKey("AIzaSyCCKcUGygE_z1Y9CEYg5MeyZmuP97mgG70")
+        GMSPlacesClient.provideAPIKey("AIzaSyCCKcUGygE_z1Y9CEYg5MeyZmuP97mgG70")
         return true
     }
 
