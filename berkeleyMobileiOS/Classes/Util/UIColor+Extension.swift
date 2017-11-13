7acf7695b77570956f4854b7994a774f489f6d68
Marisa Wong <marisawong@berkeley.edu>
2017-11-12 21:34:13 -0800

    changed map icon color for gyms, libraries, campus resources

---
+++ berkeleyMobileiOS/Classes/Util/UIColor+Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor+Extension.swift
+//  Alamofire
+//
+//  Created by Marisa Wong on 11/3/17.
+//
+import UIKit
+import Foundation
+import Swift
+
+extension UIColor {
+    public convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
+
