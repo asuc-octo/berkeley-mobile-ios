13c76901ac75f786451170cb6abbbbfadaa635b0
Anthony Kim <anthonykim02@berkeley.edu>
2018-03-19 15:53:55 -0700

    made ui skeleton, next step is to link to code

---
+++ berkeleyMobileiOS/Classes/Transit/RouteCells/RouteTitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RouteTitleTableViewCell.swift
+//  berkeleyMobileiOS
+//
+//  Created by Anthony Kim on 3/19/18.
+//  Copyright © 2018 org.berkeleyMobile. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RouteTitleTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
