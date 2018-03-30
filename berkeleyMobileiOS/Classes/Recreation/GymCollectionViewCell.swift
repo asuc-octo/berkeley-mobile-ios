0b5d27dc2dfd0709768198beec07a525ea441d56
Marisa Wong <marisawong@berkeley.edu>
2018-03-29 19:15:32 -0700

    added gyms onto my ugly gyms vc

---
+++ berkeleyMobileiOS/Classes/Recreation/GymCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GymCollectionViewCell.swift
+//  berkeleyMobileiOS
+//
+//  Created by Marisa Wong on 3/27/18.
+//  Copyright © 2018 org.berkeleyMobile. All rights reserved.
+//
+
+import UIKit
+
+class GymCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var gymImage: UIImageView!
+    
+    @IBOutlet weak var gymName: UILabel!
+    
+    @IBOutlet weak var gymStatus: UILabel!
+    
+}
