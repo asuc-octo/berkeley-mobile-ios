2c9855adebff104b837f1f62e2ec248a366cab69
Kevin Hu <kevin.hu1000@gmail.com>
2019-03-13 18:35:46 -0700

    Migrate Campus Resources and Libraries to Firebase (#133)
    * Migrate Campus Resources and Libraries to Firebase
    
    * Replace some extraneous code
    
    * Handle nil values better in Libraries and allow open times to extend to the following days
    
    * Distinguish 'by appointment' campus resources and those with no open/close hours

--- berkeleyMobileiOS/Classes/Resource/ResourceDataSource.swift
+++ berkeleyMobileiOS/Classes/Resource/ResourceDataSource.swift
@@ -13,3 +13,33 @@ protocol ResourceDataSource
     static func fetchResources(_ completion: @escaping completionHandler)
     static func parseResource(_ json: JSON) -> Resource
 }
+
+fileprivate let kOpenKey = "open_time"
+fileprivate let kCloseKey = "close_time"
+
+extension ResourceDataSource {
+    
+    // Parses the dictionary for a length 7 array of DateIntervals
+    static func parseWeeklyTimes(_ times: [[String: Any]]?) -> [DateInterval?] {
+        var parsedIntervals = [DateInterval?](repeating: nil, count: 7)
+        guard let times = times else {
+            return parsedIntervals
+        }
+        for open_close in times {
+            if let open = open_close[kOpenKey] as? Double,
+               let close = open_close[kCloseKey] as? Double,
+               let openDate = Date(timeIntervalSince1970: open).sameDayThisWeek(),
+               var closeDate = Date(timeIntervalSince1970: close).sameDayThisWeek() {
+                if openDate > closeDate {
+                    let components = Calendar.current.dateComponents([.hour, .minute, .weekday], from: closeDate)
+                    closeDate = Calendar.current.nextDate(after: openDate, matching: components, matchingPolicy: .nextTime, repeatedTimePolicy: .first, direction: .forward) ?? Date.distantPast
+                }
+                if openDate <= closeDate {
+                    let interval = DateInterval(start: openDate, end: closeDate)
+                    parsedIntervals[openDate.weekday()] = interval
+                }
+            }
+        }
+        return parsedIntervals
+    }
+}
