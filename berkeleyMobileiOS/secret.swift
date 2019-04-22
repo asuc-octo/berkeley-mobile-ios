c22a494374d58117fcf4583a55825ceec42fa397
RJ PImentel <rpimentel20@berkeley.edu>
2019-04-22 13:01:02 -0700

    Hotfix/detail view (#137)
    * fixing detail view layout
    
    * fixing detail view layout
    
    * fixed final formatting issues

--- berkeleyMobileiOS/secret.swift
+++ berkeleyMobileiOS/secret.swift
@@ -1,14 +1,12 @@
-//
+
 //  secret.swift
-//  
+//
 //
 //  Created by Marisa Wong on 4/23/18.
 //
-
 import Foundation
 
 
 let secretKAPIURL = "http://asuc-mobile-dev.herokuapp.com/api"
 
 let secretAPIKEY = "AIzaSyCMnCLU444kw1W7ultwW-lZ_rWNNScr6Q4"
-
