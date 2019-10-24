966ea450afcbb3a326234d60eb6e5bf3452b347d
RJ Pimentel <rpimentel20@.berkeley.edu>
2019-10-23 19:08:53 -0700

    purge complete

--- berkeleyMobileiOS/Classes/Transit/Controllers/CampusMapShortcutsView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CampusMapShortcutsView.swift
-//  berkeleyMobileiOS
-//
-//  Created by RJ Pimentel on 1/11/19.
-//  Copyright © 2019 org.berkeleyMobile. All rights reserved.
-//
-
---
+++ bm-persona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bm-persona
+//
+//  Created by RJ Pimentel on 10/23/19.
+//  Copyright © 2019 RJ Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
