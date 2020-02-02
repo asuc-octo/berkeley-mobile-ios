efbc33df10d53a4fbc7268a7e38bdd297283ea15
Oscar Bjorkman <oscarb@berkeley.edu>
2020-02-02 14:03:07 -0800

    Begin calendar

---
+++ bm-persona/Calendar/CalendarDataSource/CalendarDataSource.swift
@@ -0,0 +1,38 @@
+//
+//  CalendarDataSource.swift
+//  bm-persona
+//
+//  Created by Oscar Bjorkman on 2/2/20.
+//  Copyright © 2020 RJ Pimentel. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+fileprivate let kCampusResourcesEndpoint = "Academic Calendar Events"
+
+class CalendarDataSource: DataSource {
+    
+    // Fetch the list of campus resources and report back to the completionHandler.
+    static func fetchItems(_ completion: @escaping DataSource.completionHandler)
+    {
+        let db = Firestore.firestore()
+        db.collection(kCampusResourcesEndpoint).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+                return
+            } else {
+                let calendar = querySnapshot!.documents.map { (document) -> CalendarEntry in
+                    let dict = document.data()
+                    return parseCalendarResource(dict)
+                }
+                completion(calendar)
+            }
+        }
+    }
+    
+    // Return a CalendaryEntry object parsed from a dictionary.
+    private static func parseCalendarResource(_ dict: [String: Any]) -> CalendarEntry {
+        
+    }
+}
---
+++ bm-persona/Calendar/CalendarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarViewController.swift
+//  bm-persona
+//
+//  Created by Oscar Bjorkman on 2/2/20.
+//  Copyright © 2020 RJ Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
