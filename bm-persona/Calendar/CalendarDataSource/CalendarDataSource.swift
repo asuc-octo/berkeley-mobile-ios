86101771048b9921ac04ca23e77ede66d5e78fb9
Oscar Bjorkman <oscarb@berkeley.edu>
2020-02-11 15:56:40 -0800

    add calendar firebase

--- bm-persona/Calendar/CalendarDataSource/CalendarDataSource.swift
+++ bm-persona/Calendar/CalendarDataSource/CalendarDataSource.swift
@@ -33,7 +33,10 @@ class CalendarDataSource: DataSource {
     
     // Return a CalendaryEntry object parsed from a dictionary.
     private static func parseCalendarResource(_ dict: [String: Any]) -> CalendarEntry {
-        print(dict)
-        return CalendarEntry(name: "Testing name", campusLocation: "Somewhere", date: Date(), eventType: "L&S Advising")
+        let entry = CalendarEntry(name: dict["event_name"] as? String ?? "Unnamed",
+                                  campusLocation: "TBD",
+                                  date: Date(timeIntervalSince1970: dict["event_date"] as? Double ?? 0),
+                                  eventType: dict["event_type"] as? String ?? "Uncategorized")
+        return entry
     }
 }
--- bm-persona/Calendar/CalendarViewController.swift
+++ bm-persona/Calendar/CalendarViewController.swift
@@ -8,25 +8,105 @@
 
 import UIKit
 
+fileprivate let kCardPadding: UIEdgeInsets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+fileprivate let kViewMargin: CGFloat = 128
+
+
 class CalendarViewController: UIViewController {
+    private var scrollView: UIScrollView!
+    
+    private var eventsLabel: UILabel!
+    
+    private var calendarTable: UITableView!
+    private var calendarCard: CardView!
+    
+    private var calendarEntries: [CalendarEntry] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        self.view.backgroundColor = UIColor.blue
+        self.view.layoutMargins = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        self.view.backgroundColor = Color.modalBackground
+        
+        setupHeader()
+        setupScrollView()
+        setupCalendarList()
+        
+        DataManager.shared.fetch(source: CalendarDataSource.self) { calendarEntries in
+            self.calendarEntries = calendarEntries as? [CalendarEntry] ?? []
+            self.calendarTable.reloadData()
+        }
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+
+// Dummy Data
+extension CalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calendarEntries.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let nameLabel = UILabel()
+        cell.addSubview(nameLabel)
+        nameLabel.text = calendarEntries[indexPath.row].name
+        nameLabel.sizeToFit()
+        return cell
     }
-    */
+    
+}
 
+extension CalendarViewController {
+    // Events Label and Blobs
+    func setupHeader() {
+        eventsLabel = UILabel()
+        eventsLabel.font = Font.bold(24)
+        eventsLabel.text = "Events"
+        eventsLabel.translatesAutoresizingMaskIntoConstraints = false
+        eventsLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor).isActive = true
+        eventsLabel.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor).isActive = true
+    }
+    
+    // ScrollView
+    func setupScrollView() {
+        scrollView = UIScrollView()
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.setConstraintsToView(top: view, bottom: view, left: view, right: view)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.contentSize.height = calendarCard.frame.maxY + view.layoutMargins.bottom
+    }
+    
+    // Calendar Table
+    func setupCalendarList() {
+        let card = CardView()
+        card.layoutMargins = kCardPadding
+        scrollView.addSubview(card)
+        card.translatesAutoresizingMaskIntoConstraints = false
+        card.topAnchor.constraint(equalTo: view.topAnchor, constant: kViewMargin).isActive = true
+        card.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor).isActive = true
+        card.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor).isActive = true
+        card.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor).isActive = true
+        
+        let table = UITableView()
+        table.delegate = self
+        table.dataSource = self
+        scrollView.addSubview(table)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.topAnchor.constraint(equalTo: card.layoutMarginsGuide.topAnchor).isActive = true
+        table.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
+        table.rightAnchor.constraint(equalTo: card.layoutMarginsGuide.rightAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: card.layoutMarginsGuide.bottomAnchor).isActive = true
+
+        calendarCard = card
+        calendarTable = table
+    }
 }
