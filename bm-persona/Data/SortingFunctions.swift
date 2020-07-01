a871e40f9f1c4d4e9b6015675aa76399cf735800
Kevin Hu <kevin.hu@berkeley.edu>
2020-06-30 18:57:15 -0700

    Change sortClose to sort by name on tie (#180)

--- bm-persona/Data/SortingFunctions.swift
+++ bm-persona/Data/SortingFunctions.swift
@@ -20,7 +20,11 @@ class SortingFunctions {
         }
         let d1 = loc1.getDistanceToUser(userLoc: currLocation!)
         let d2 = loc2.getDistanceToUser(userLoc: currLocation!)
-        if d2.isNaN {
+        if d1 == d2 || d1.isNaN && d2.isNaN,
+            let loc1 = loc1 as? SearchItem,
+            let loc2 = loc2 as? SearchItem {
+            return sortAlph(loc1: loc1, loc2: loc2)
+        } else if d2.isNaN {
             return true
         } else if d1.isNaN {
             return false
