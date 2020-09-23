f8a19fe5c2711af80792e55cc1723eaf5df46513
Oscar Bjorkman <oscarb@berkeley.edu>
2020-09-22 21:47:20 -0700

    Move device extension to utils

--- berkeley-mobile/Calendar/CalendarViewController.swift
+++ berkeley-mobile/Calendar/CalendarViewController.swift
@@ -228,15 +228,3 @@ extension CalendarViewController {
     }
 }
 
-extension UIDevice {
-    var hasNotch: Bool {
-        if #available(iOS 11.0, *) {
-            if UIApplication.shared.windows.count == 0 { return false }
-            let top = UIApplication.shared.windows[0].safeAreaInsets.top
-            return top > 20
-        } else {
-            // Fallback on earlier versions
-            return false
-        }
-    }
-}
