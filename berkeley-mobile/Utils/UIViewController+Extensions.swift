ea64901c204fe2cafe3ea82f44e9a483427f3083
Oscar Bjorkman <oscar@oscarbjorkman.com>
2020-10-03 16:15:33 -0700

    Add clickable attributes, adjust Gesture recg

--- berkeley-mobile/Utils/UIViewController+Extensions.swift
+++ berkeley-mobile/Utils/UIViewController+Extensions.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import CoreLocation
+import MapKit
 
 extension UIViewController {
     public func add(child: UIViewController, frame: CGRect = .zero) {
@@ -33,6 +35,44 @@ extension UIViewController {
             } else {
                 alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { _ in
                     UIApplication.shared.open(website_url, options: [:])
+                }))
+            }
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // Presents an alert to open phone number
+    public func presentAlertLinkTelephone(title: String, message: String, options: String..., phoneNumber: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        guard let number = URL(string: "tel://\(phoneNumber)") else { return }
+        
+        for (index, option) in options.enumerated() {
+            if (index == 0) {
+                alertController.addAction(UIAlertAction.init(title: option, style: .cancel))
+            } else {
+                alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { _ in
+                    UIApplication.shared.open(number, options: [:])
+                }))
+            }
+        }
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // Presents an alert to open coordinates
+    public func presentAlertLinkMaps(title: String, message: String, options: String..., lat: CLLocationDegrees, lon: CLLocationDegrees, name: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let coordinates = CLLocationCoordinate2DMake(lat, lon)
+        let placemark = MKPlacemark(coordinate: coordinates, addressDictionary: nil)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "\(name)"
+        
+        for (index, option) in options.enumerated() {
+            if (index == 0) {
+                alertController.addAction(UIAlertAction.init(title: option, style: .cancel))
+            } else {
+                alertController.addAction(UIAlertAction.init(title: option, style: .default, handler: { _ in
+                    mapItem.openInMaps(launchOptions: [:])
                 }))
             }
         }
