c1aec937dd64256b64b55116e4706c6c0ce2c653
Oscar Bjorkman <oscar@oscarbjorkman.com>
2021-01-23 12:52:08 -0800

    more changes

--- berkeley-mobile/StudyPact/ProfileViewController.swift
+++ berkeley-mobile/StudyPact/ProfileViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    private var loggedIn = false
+    
     private var profileLabel: UILabel!
 
     override func viewDidLoad() {
@@ -19,6 +21,7 @@ class ProfileViewController: UIViewController {
         self.view.backgroundColor = Color.modalBackground
         
         setupHeader()
+        setupProfile()
     }
     
 }
@@ -34,5 +37,74 @@ extension ProfileViewController {
         profileLabel.translatesAutoresizingMaskIntoConstraints = false
         profileLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 15).isActive = true
         profileLabel.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor).isActive = true
+    }
+    
+    func setupProfile() {
+        let initials = UILabel()
+        initials.text = "OB"
+        initials.textAlignment = .center
+        initials.font = Font.bold(40)
+        initials.layer.cornerRadius = 50
+        initials.layer.masksToBounds = true
+        initials.backgroundColor = Color.lightGrayText
+        initials.textColor = UIColor.white
+        
+        view.addSubview(initials)
+        
+        initials.translatesAutoresizingMaskIntoConstraints = false
+        initials.topAnchor.constraint(equalTo: profileLabel.bottomAnchor, constant: 15).isActive = true
+        initials.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        initials.setHeightConstraint(100)
+        initials.setWidthConstraint(100)
+                
+        let hstack = UIStackView()
+        hstack.axis = .horizontal
+        hstack.distribution = .equalCentering
+        hstack.alignment = .center
+        
+        view.addSubview(hstack)
+        
+        hstack.translatesAutoresizingMaskIntoConstraints = false
+        hstack.topAnchor.constraint(equalTo: initials.bottomAnchor, constant: 15).isActive = true
+        hstack.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 15).isActive = true
+        hstack.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -15).isActive = true
+        
+        let lvstack = UIStackView()
+        lvstack.axis = .vertical
+        lvstack.distribution = .fillEqually
+        lvstack.alignment = .center
+        
+        let rvstack = UIStackView()
+        lvstack.axis = .vertical
+        rvstack.distribution = .fillEqually
+        rvstack.alignment = .center
+        
+        hstack.addArrangedSubview(lvstack)
+        hstack.addArrangedSubview(rvstack)
+        
+        // TODO: - Dynamic field data
+        let firstnameLabel = UILabel()
+        firstnameLabel.text = "First Name"
+        firstnameLabel.font = Font.medium(12)
+        firstnameLabel.textAlignment = .left
+        firstnameLabel.textColor = Color.blackText
+        firstnameLabel.numberOfLines = 1
+        firstnameLabel.adjustsFontSizeToFitWidth = true
+        firstnameLabel.minimumScaleFactor = 0.7
+        
+        let firstnameField = BorderedTextField(text: "OB")
+        
+        
+        lvstack.addArrangedSubview(firstnameLabel)
+        rvstack.addArrangedSubview(firstnameField)
+        
+        firstnameField.leftAnchor.constraint(equalTo: firstnameLabel.rightAnchor).isActive = true
+        
+        firstnameLabel.translatesAutoresizingMaskIntoConstraints = false
+        firstnameLabel.setHeightConstraint(36)
+        firstnameLabel.setWidthConstraint(100)
+        
+        firstnameField.translatesAutoresizingMaskIntoConstraints = false
+        firstnameField.setHeightConstraint(36)
     }
 }
