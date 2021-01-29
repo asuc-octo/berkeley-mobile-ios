4c68ab69d2ce9dfb1f6ca99a391d1c601f98ac33
Eashan Mathur <eashanmathur@gmail.com>
2021-01-28 18:10:41 -0800

    Eashan study pact onboarding (#232)
    * Added onboarding flow for StudyPact
    
    * Fixing up some stuff
    
    * minor changes, style, directories
    
    * add moved files
    
    * Updated spacing
    
    * removed comment
    
    * crop images, stack spacing
    
    * change messages, align labels
    
    * remove wip
    
    * spacing stuff, moved get started button to only last view
    
    * fix spacing
    
    * fix folders, spacingF
    
    Co-authored-by: Shawn Huang <huangshawn1@gmail.com>

--- berkeley-mobile/Assets/Colors/Colors+StudyPact.swift
+++ berkeley-mobile/Assets/Colors/Colors+StudyPact.swift
@@ -2,14 +2,27 @@
 //  Colors+StudyPact.swift
 //  berkeley-mobile
 //
-//  Created by Shawn Huang on 1/22/21.
-//  Copyright © 2021 ASUC OCTO. All rights reserved.
+//  Created by Eashan Mathur on 11/14/20.
+//  Copyright © 2020 ASUC OCTO. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
 extension Color {
+
+    static var onboardingTint: UIColor {
+        return UIColor(red: 0.337254902, green: 0.4352941176, blue: 0.7254901961, alpha: 1)
+    }
+    
+    static var pageViewBackgroundTint: UIColor {
+        return UIColor(displayP3Red: 86/255, green: 111/255, blue: 185/255, alpha: 1)
+    }
+    
+    static var getStartedButton: UIColor {
+        return UIColor(red: 0.3191883862, green: 0.4302733541, blue: 0.7476411462, alpha: 1)
+    }
+    
     struct StudyGroups {
         static var createGroupDottedBorder: UIColor {
             return UIColor(red: 158.0 / 255.0, green: 158.0 / 255.0, blue: 158.0 / 255.0, alpha: 1.0)
---
+++ berkeley-mobile/StudyPact/Onboarding/StudyPactOnboardingViewController.swift
@@ -0,0 +1,131 @@
+//
+//  StudyPactOnboardingViewController.swift
+//  berkeley-mobile
+//
+//  Created by Eashan Mathur on 10/17/20.
+//  Copyright © 2020 ASUC OCTO. All rights reserved.
+//
+
+import UIKit
+
+
+class StudyPactOnboardingViewController: UIViewController {
+    let signUpLabel: UILabel = {
+        let label = UILabel()
+        label.font = Font.bold(36)
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let bearImage = UIImageView()
+    
+    let numberImage: UIImageView = {
+        let numberImage = UIImageView()
+        numberImage.contentMode = .scaleAspectFit
+        numberImage.translatesAutoresizingMaskIntoConstraints = false
+        return numberImage
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.font = Font.regular(18)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textColor = .gray
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return descriptionLabel
+    }()
+    
+    let centeringView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let getStarted: UIButton = {
+        let button = UIButton()
+        button.setTitle("Let's get started!", for: .normal)
+        button.titleLabel?.font = Font.bold(15)
+        button.backgroundColor = Color.getStartedButton
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(getStarted(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLayoutSubviews() {
+        getStarted.layer.cornerRadius = getStarted.frame.height / 2
+    }
+    
+    @objc func getStarted(_: UIButton) {
+        if let tabBarController = UIApplication.shared.windows.first!.rootViewController as? TabBarController {
+            tabBarController.selectProfileTab()
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    var hasGetStarted = false
+    
+    init(_signUpLabelText: String, _bearImage: UIImage, _numberImage: UIImage, _descriptionText: String, _hasGetStarted: Bool = false) {
+        signUpLabel.text = _signUpLabelText
+        bearImage.image = _bearImage
+        numberImage.image = _numberImage
+        descriptionLabel.text = _descriptionText
+        self.hasGetStarted = _hasGetStarted
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupCard()
+    }
+    
+    func setupCard(){
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.distribution = .equalSpacing
+        stack.alignment = .center
+        stack.spacing = 30
+        view.addSubview(stack)
+        
+        let numberTitleStack = UIStackView()
+        numberTitleStack.axis = .horizontal
+        numberTitleStack.distribution = .equalCentering
+        numberTitleStack.alignment = .center
+        numberTitleStack.spacing = 14
+        
+        numberTitleStack.addArrangedSubview(numberImage)
+        numberTitleStack.addArrangedSubview(signUpLabel)
+        
+        stack.addArrangedSubview(bearImage)
+        stack.addArrangedSubview(numberTitleStack)
+        stack.addArrangedSubview(descriptionLabel)
+        
+        bearImage.contentMode = .scaleAspectFit
+        signUpLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.frame.height * 0.05).isActive = true
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        bearImage.translatesAutoresizingMaskIntoConstraints = false
+        stack.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        stack.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        
+        if self.hasGetStarted {
+            view.addSubview(getStarted)
+            view.addSubview(centeringView)
+            centeringView.topAnchor.constraint(equalTo: stack.bottomAnchor).isActive = true
+            centeringView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+            getStarted.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            getStarted.heightAnchor.constraint(equalToConstant: 43).isActive = true
+            getStarted.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+            getStarted.centerYAnchor.constraint(equalTo: centeringView.centerYAnchor).isActive = true
+        }
+    }
+    
+}
