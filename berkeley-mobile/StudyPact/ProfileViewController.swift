e2e1397c025d52ec7c9e791d84956a64a109ffca
Oscar Bjorkman <oscar@oscarbjorkman.com>
2021-01-29 19:25:47 -0800

    add profile image editing

--- berkeley-mobile/StudyPact/ProfileViewController.swift
+++ berkeley-mobile/StudyPact/ProfileViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, UITextFieldDelegate {
+class ProfileViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     private var loggedIn = false
     
     private var profileLabel: UILabel!
+    private var initialsLabel: UILabel!
+    private var profileImageView: UIImageView!
+    private var profileImage: UIImage! {
+        didSet {
+            profileImageView.image = profileImage
+            initialsLabel.isHidden = true
+            profileImageView.isHidden = false
+        }
+    }
+    private var changeImageButton: UIButton!
+    private var imagePicker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +36,12 @@ class ProfileViewController: UIViewController, UITextFieldDelegate {
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        
+        setupProfileImage()
     }
     
     @objc func dismissKeyboard() {
@@ -81,7 +98,53 @@ extension ProfileViewController {
         initials.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         initials.setHeightConstraint(100)
         initials.setWidthConstraint(100)
-                
+        
+        initialsLabel = initials
+        
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 50
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.isHidden = true
+        
+        view.addSubview(imageView)
+
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: profileLabel.bottomAnchor, constant: 15).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.setHeightConstraint(100)
+        imageView.setWidthConstraint(100)
+        
+        profileImageView = imageView
+        
+        let regularAttributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.underlineStyle: 1,
+            NSAttributedString.Key.font: Font.medium(14),
+            NSAttributedString.Key.foregroundColor: Color.lightGrayText
+        ]
+        
+        let pressedAttributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.underlineStyle: 1,
+            NSAttributedString.Key.font: Font.medium(14),
+            NSAttributedString.Key.foregroundColor: Color.lightLightGrayText
+        ]
+        
+        let button = UIButton()
+        let attributedString = NSMutableAttributedString(string: "Edit Profile Picture", attributes: regularAttributes)
+        button.setAttributedTitle(attributedString, for: .normal)
+        
+        let pressedAttributedString = NSMutableAttributedString(string: "Edit Profile Picture", attributes: pressedAttributes)
+        button.setAttributedTitle(pressedAttributedString, for: .selected)
+        
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.topAnchor.constraint(equalTo: initials.bottomAnchor, constant: 15).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.setHeightConstraint(18)
+        button.addTarget(self, action: #selector(changeImageButtonPressed), for: .touchUpInside)
+        
+        changeImageButton = button
+        
         let hstack = UIStackView()
         hstack.axis = .horizontal
         hstack.distribution = .equalCentering
@@ -90,7 +153,7 @@ extension ProfileViewController {
         view.addSubview(hstack)
         
         hstack.translatesAutoresizingMaskIntoConstraints = false
-        hstack.topAnchor.constraint(equalTo: initials.bottomAnchor, constant: 15).isActive = true
+        hstack.topAnchor.constraint(equalTo: changeImageButton.bottomAnchor, constant: 30).isActive = true
         hstack.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 15).isActive = true
         hstack.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -15).isActive = true
         
@@ -148,30 +211,50 @@ extension ProfileViewController {
         phoneLabel.adjustsFontSizeToFitWidth = true
         phoneLabel.minimumScaleFactor = 0.7
         
+        let facebookLabel = UILabel()
+        facebookLabel.text = "Facebook URL"
+        facebookLabel.font = Font.medium(14)
+        facebookLabel.textAlignment = .left
+        facebookLabel.textColor = Color.blackText
+        facebookLabel.numberOfLines = 1
+        facebookLabel.adjustsFontSizeToFitWidth = true
+        facebookLabel.minimumScaleFactor = 0.7
+        
         let firstnameField = BorderedTextField(text: "Oski")
         let lastnameField = BorderedTextField(text: "Bear")
         let emailField = BorderedTextField(text: "oskibear@berkeley.edu")
         let phoneField = BorderedTextField()
+        let facebookField = BorderedTextField(text: "www.facebook.com/123456789")
         
         firstnameField.delegate = self
         lastnameField.delegate = self
         emailField.delegate = self
         phoneField.delegate = self
+        facebookField.delegate = self
+        
+        firstnameField.autocorrectionType = .no
+        lastnameField.autocorrectionType = .no
+        phoneField.keyboardType = .phonePad
+        facebookField.autocorrectionType = .no
+        facebookField.keyboardType = .URL
         
         lvstack.addArrangedSubview(firstnameLabel)
         lvstack.addArrangedSubview(lastnameLabel)
         lvstack.addArrangedSubview(emailLabel)
         lvstack.addArrangedSubview(phoneLabel)
+        lvstack.addArrangedSubview(facebookLabel)
 
         rvstack.addArrangedSubview(firstnameField)
         rvstack.addArrangedSubview(lastnameField)
         rvstack.addArrangedSubview(emailField)
         rvstack.addArrangedSubview(phoneField)
+        rvstack.addArrangedSubview(facebookField)
         
         firstnameField.leftAnchor.constraint(equalTo: firstnameLabel.rightAnchor, constant: 8).isActive = true
         lastnameField.leftAnchor.constraint(equalTo: lastnameLabel.rightAnchor, constant: 8).isActive = true
         emailField.leftAnchor.constraint(equalTo: emailLabel.rightAnchor, constant: 8).isActive = true
         phoneField.leftAnchor.constraint(equalTo: phoneLabel.rightAnchor, constant: 8).isActive = true
+        facebookField.leftAnchor.constraint(equalTo: facebookLabel.rightAnchor, constant: 8).isActive = true
         
         firstnameField.rightAnchor.constraint(equalTo: rvstack.rightAnchor).isActive = true
         
@@ -202,21 +285,34 @@ extension ProfileViewController {
         phoneField.translatesAutoresizingMaskIntoConstraints = false
         phoneField.setHeightConstraint(36)
         
-        let facebookButton = ActionButton(title: "Link Facebook Profile", font: Font.bold(14))
-        facebookButton.addTarget(self, action: #selector(facebookButtonPressed), for: .touchUpInside)
-        facebookButton.setHeightConstraint(40)
+        facebookLabel.translatesAutoresizingMaskIntoConstraints = false
+        facebookLabel.setHeightConstraint(36)
         
-        view.addSubview(facebookButton)
-        
-        facebookButton.translatesAutoresizingMaskIntoConstraints = false
-        facebookButton.topAnchor.constraint(equalTo: hstack.bottomAnchor, constant: 30).isActive = true
-        facebookButton.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 18).isActive = true
-        facebookButton.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -18).isActive = true
-        
+        facebookField.translatesAutoresizingMaskIntoConstraints = false
+        facebookField.setHeightConstraint(36)
     }
     
-    @objc private func facebookButtonPressed(sender: UIButton) {
-        // TODO: - Implement this feature
-        print("Link facebook pressed")
+    @objc private func changeImageButtonPressed(sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func setupProfileImage() {
+        // TODO: - Check backend for image
+        
+    }
+}
+
+extension ProfileViewController {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            // TODO: - Save/update image in backend
+            profileImageView.contentMode = .scaleAspectFill
+            profileImage = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
