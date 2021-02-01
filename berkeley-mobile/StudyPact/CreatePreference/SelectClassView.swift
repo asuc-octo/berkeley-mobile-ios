1edbd23952f5562b8e7b190968f2af5dc564f798
Shawn Huang <huangshawn1@gmail.com>
2021-01-31 20:07:49 -0800

    api call to get all courses

--- berkeley-mobile/StudyPact/CreatePreference/SelectClassView.swift
+++ berkeley-mobile/StudyPact/CreatePreference/SelectClassView.swift
@@ -11,6 +11,7 @@ import UIKit
 class SelectClassView: UIView, UITextFieldDelegate, EnableNextDelegate {
     let preferenceVC: CreatePreferenceViewController
     var textField: MaterialTextField!
+    var classNames: [String] = []
     override func layoutSubviews() {
         super.layoutSubviews()
     }
@@ -19,6 +20,7 @@ class SelectClassView: UIView, UITextFieldDelegate, EnableNextDelegate {
         self.preferenceVC = preferenceVC
         super.init(frame: .zero)
         setUpView()
+        loadClasses()
     }
     
     required init?(coder: NSCoder) {
@@ -46,6 +48,32 @@ class SelectClassView: UIView, UITextFieldDelegate, EnableNextDelegate {
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.heightAnchor.constraint(equalToConstant: 35).isActive = true
         textField.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+    }
+    
+    func loadClasses() {
+        guard self.classNames.count == 0,
+              let url = URL(string: "https://berkeleytime.com/api/catalog/catalog_json/") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            guard let data = data, let response = response,
+                  let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200,
+                  error == nil else { return }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data) as! Dictionary<String, AnyObject>
+                let courses = json["courses"] as! Array<Dictionary<String, AnyObject>>
+                for course in courses {
+                    guard let courseNumber = course["course_number"] as? String,
+                          let abbreviation = course["abbreviation"] as? String else { continue }
+                    self.classNames.append(abbreviation + " " + courseNumber)
+                }
+            } catch {
+                print("error fetching courses")
+            }
+        })
+        task.resume()
     }
     
     @objc func setNextEnabled() {
