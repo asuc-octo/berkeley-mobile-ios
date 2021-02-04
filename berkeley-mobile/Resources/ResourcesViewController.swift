b7ba19b94727b2529e4341405fa0a15781857736
Shawn Huang <huangshawn1@gmail.com>
2021-02-04 14:44:42 -0800

    remove bear button

--- berkeley-mobile/Resources/ResourcesViewController.swift
+++ berkeley-mobile/Resources/ResourcesViewController.swift
@@ -13,8 +13,6 @@ fileprivate let kViewMargin: CGFloat = 16
 
 class ResourcesViewController: UIViewController {
     private var resourcesLabel: UILabel!
-
-    private var bearPactAccessButton: UIButton!
     
     private var resourcesCard: CardView!
     private var resourcesTable: FilterTableView = FilterTableView<Resource>(frame: .zero, tableFunctions: [], defaultSort: SortingFunctions.sortAlph(item1:item2:))
@@ -61,24 +59,6 @@ extension ResourcesViewController {
         // Hacky workaround. Assumes that it is safe to overlap the text with half (and some) of the blob.
         blobView.centerXAnchor.constraint(equalTo: resourcesLabel.rightAnchor, constant: -20).isActive = true
         blobImageView = blobView
-        
-        #if DEBUG
-        bearPactAccessButton = UIButton()
-        bearPactAccessButton.setTitle("🐻", for: .normal)
-        bearPactAccessButton.frame = CGRect(x: 0, y: 0, width: 50, height: 30)
-        view.addSubview(bearPactAccessButton)
-        bearPactAccessButton.translatesAutoresizingMaskIntoConstraints = false
-        bearPactAccessButton.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor).isActive = true
-        bearPactAccessButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: -5).isActive = true
-        bearPactAccessButton.addTarget(self, action: #selector(self.pressedBear(_:)), for: .touchUpInside)
-        #endif
-
-    }
-    
-    @objc func pressedBear(_ sender: UIButton) {
-        let vc = PageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
-        vc.modalPresentationStyle = .fullScreen
-        self.present(vc, animated: true, completion: nil)
     }
     
     // SegmentedControl and Page views
