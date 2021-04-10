102c60f40a91b66a5fd247e196614b4500c3001b
Kevin Wang <kev_wang@berkeley.edu>
2021-04-10 16:06:44 -0700

    disabled zooming and vertical indicator, also created some spacing

--- berkeley-mobile/Resources/SproulClub/SproulClubViewController.swift
+++ berkeley-mobile/Resources/SproulClub/SproulClubViewController.swift
@@ -10,25 +10,33 @@ import Foundation
 import UIKit
 import WebKit
 
-class SproulClubViewController: UIViewController, WKUIDelegate {
+class SproulClubViewController: UIViewController, WKUIDelegate, UIScrollViewDelegate {
     var webView: WKWebView!
         
-        override func loadView() {
+        override func viewDidLoad() {
+            super.viewDidLoad()
             let webConfiguration = WKWebViewConfiguration()
             webView = WKWebView(frame: .zero, configuration: webConfiguration)
             webView.uiDelegate = self
-            view = webView
-        }
-    
-        override func viewDidLoad() {
-            super.viewDidLoad()
+            webView.scrollView.delegate = self
+            
+            webView.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(webView)
+            webView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 6).isActive = true
+            webView.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 6).isActive = true
+            webView.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: -6).isActive = true
+            webView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20).isActive = true
+            
             let myURL = URL(string:"https://www.sproul.club/")
             let myRequest = URLRequest(url: myURL!)
             webView.load(myRequest)
             
-            //disable zooming in
+            //disabling zooming and vertical scroll indicator
             webView.scrollView.minimumZoomScale = 1.0
             webView.scrollView.maximumZoomScale = 1.0
+            webView.scrollView.showsVerticalScrollIndicator = false
+            webView.scrollView.pinchGestureRecognizer?.isEnabled = false
+            
         }
     
 }
