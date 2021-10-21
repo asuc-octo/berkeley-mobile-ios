0e97a5ed5d4f2210948cc3ee47f65528f8e3d123
Kevin Wang <kev_wang@berkeley.edu>
2021-10-20 18:37:58 -0700

    fix

--- berkeley-mobile/Map/MapViewController.swift
+++ berkeley-mobile/Map/MapViewController.swift
@@ -221,6 +221,8 @@ class MapViewController: UIViewController, SearchDrawerViewDelegate {
     
     var workItem: DispatchWorkItem?
     private func updateMapMarkers() {
+        self.previousMapMarker = nil
+        self.previousPlaceMark = nil
         workItem?.cancel()
         let selectedIndices = filterView.indexPathsForSelectedItems?.map { $0.row }
         workItem = Filter.satisfiesAny(filters: filters, on: mapMarkers, indices: selectedIndices, completion: {
