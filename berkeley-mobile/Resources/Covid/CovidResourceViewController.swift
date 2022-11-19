58a13f3981a7947dd3672886719b0de245bd009e
chloeyeo <chloeyeo@berkeley.edu>
2022-11-18 18:10:22 -0800

    safety card attempt

--- berkeley-mobile/Resources/Covid/CovidResourceViewController.swift
+++ berkeley-mobile/Resources/Covid/CovidResourceViewController.swift
@@ -37,10 +37,10 @@ class CovidResourceViewController: UIViewController {
         DataManager.shared.fetch(source: CovidResourceDataSource.self) { resourceEntries in
             guard let covidData = resourceEntries[0] as? CovidResource else { return }
                         
-            self.screeningUrl = covidData.dailyScreeningLink
-            self.valueOne.text = covidData.dailyIncrease
-            self.valueTwo.text = covidData.totalCases
-            self.lastUpdated.text = "Last Updated: \(covidData.lastUpdated)"
+//            self.screeningUrl = covidData.dailyScreeningLink
+//            self.valueOne.text = covidData.dailyIncrease
+//            self.valueTwo.text = covidData.totalCases
+//            self.lastUpdated.text = "Last Updated: \(covidData.lastUpdated)"
         }
     }
 }
@@ -73,56 +73,68 @@ extension CovidResourceViewController {
         
         let headerLabel = UILabel()
         headerLabel.font = Font.bold(24)
-        headerLabel.text = "Statistics"
+        headerLabel.text = "Bear Walk"
+        
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = "Visit website at: https://bearwalk.ridecell.com/request"
+        subtitleLabel.font = Font.regular(16)
+        subtitleLabel.numberOfLines = 1
+        subtitleLabel.adjustsFontSizeToFitWidth = true
+        subtitleLabel.minimumScaleFactor = 0.7
+        subtitleLabel.textAlignment = .left
+        subtitleLabel.textColor = Color.blackText
         
         card.addSubview(headerLabel)
+        card.addSubview(subtitleLabel)
                 
         headerLabel.translatesAutoresizingMaskIntoConstraints = false
         headerLabel.topAnchor.constraint(equalTo: card.layoutMarginsGuide.topAnchor).isActive = true
         headerLabel.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
         headerLabel.rightAnchor.constraint(equalTo: card.layoutMarginsGuide.rightAnchor).isActive = true
         
-        let stack = UIStackView()
-        stack.axis = .horizontal
-        stack.distribution = .fillEqually
-        stack.alignment = .center
-        stack.spacing = 9
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 12).isActive = true
+        subtitleLabel.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
+        subtitleLabel.rightAnchor.constraint(equalTo: card.layoutMarginsGuide.rightAnchor).isActive = true
         
-        let (positiveTests, positiveLabel) = createOverviewCard(cardHeader: "Total")
-        let (uhsTests, uhsLabel) = createOverviewCard(cardHeader: "New")
+        let callToBookButton = ActionButton(title: "Call To Book", font: Font.regular(12))
+        callToBookButton.addTarget(self, action: #selector(appointmentButtonPressed), for: .touchUpInside)
         
-        stack.addArrangedSubview(positiveTests)
-        stack.addArrangedSubview(uhsTests)
+        card.addSubview(callToBookButton)
         
-        stack.translatesAutoresizingMaskIntoConstraints = false
+        callToBookButton.translatesAutoresizingMaskIntoConstraints = false
+        callToBookButton.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
+        callToBookButton.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 12).isActive = true
         
-        card.addSubview(stack)
+        card.bottomAnchor.constraint(equalTo: callToBookButton.bottomAnchor, constant: kViewMargin).isActive = true
         
-        stack.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: kViewMargin).isActive = true
-        stack.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
-        stack.rightAnchor.constraint(equalTo: card.layoutMarginsGuide.rightAnchor).isActive = true
         
-        let updatedLabel = UILabel()
-        updatedLabel.font = Font.light(14)
-        updatedLabel.textColor = Color.lightGrayText
-        updatedLabel.textAlignment = .left
-        updatedLabel.text = "Last Updated: "
-        
-        card.addSubview(updatedLabel)
-        
-        updatedLabel.translatesAutoresizingMaskIntoConstraints = false
-        updatedLabel.topAnchor.constraint(equalTo: stack.bottomAnchor, constant: kViewMargin).isActive = true
-        updatedLabel.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
-        updatedLabel.rightAnchor.constraint(equalTo: card.layoutMarginsGuide.rightAnchor).isActive = true
-        
-        card.bottomAnchor.constraint(equalTo: updatedLabel.bottomAnchor, constant: kViewMargin).isActive = true
-        
-        overviewStack = stack
+//        let stack = UIStackView()
+//        stack.axis = .horizontal
+//        stack.distribution = .fillEqually
+//        stack.alignment = .center
+//        stack.spacing = 9
+//
+//        let (positiveTests, positiveLabel) = createOverviewCard(cardHeader: "Total")
+//        let (uhsTests, uhsLabel) = createOverviewCard(cardHeader: "New")
+//
+//        stack.addArrangedSubview(positiveTests)
+//        stack.addArrangedSubview(uhsTests)
+//
+//        stack.translatesAutoresizingMaskIntoConstraints = false
+//
+//        card.addSubview(stack)
+//
+//        stack.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: kViewMargin).isActive = true
+//        stack.leftAnchor.constraint(equalTo: card.layoutMarginsGuide.leftAnchor).isActive = true
+//        stack.rightAnchor.constraint(equalTo: card.layoutMarginsGuide.rightAnchor).isActive = true
+                
+//        overviewStack = stack
         overviewCard = card
         
-        valueOne = uhsLabel
-        valueTwo = positiveLabel
-        lastUpdated = updatedLabel
+//        valueOne = uhsLabel
+//        valueTwo = positiveLabel
+//        lastUpdated = updatedLabel
     }
     
     func createOverviewCard(cardHeader: String, cardValue: Int? = nil) -> (UIView, UILabel) {
