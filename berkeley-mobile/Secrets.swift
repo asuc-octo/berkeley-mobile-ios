2972280ec8abbc08f11d83bc067714d53b68897b
Minjae Kwak <mjkwak0184@gmail.com>
2023-04-28 18:48:29 -0700

    Updated project structure

---
+++ berkeley-mobile/Secrets.swift
@@ -0,0 +1,25 @@
+//
+//  Secrets.xcconfig
+//  berkeley-mobile
+//
+//  Created by Kevin Hu on 2/2/21.
+//  Copyright © 2021 ASUC OCTO. All rights reserved.
+//
+
+// Configuration settings file format documentation can be found at:
+// https://help.apple.com/xcode/#/dev745c5c974
+
+let API_URLS = [
+    "REGISTER_USER": "https://46s697sdt2.execute-api.us-west-1.amazonaws.com/default/RegisterUser",
+    "AUTHENTICATE_USER": "https://f1d7i6gsxj.execute-api.us-west-1.amazonaws.com/default/AuthenticateUser",
+    "GET_USER": "https://2uwb7d3csg.execute-api.us-east-1.amazonaws.com/GetUser",
+    "ADD_USER": "https://2uwb7d3csg.execute-api.us-east-1.amazonaws.com/AddNewUser",
+    "ADD_CLASS": "https://2uwb7d3csg.execute-api.us-east-1.amazonaws.com/AddClass",
+    "CANCEL_PENDING": "https://2uwb7d3csg.execute-api.us-east-1.amazonaws.com/CancelPendingClassRequest",
+    "LEAVE_GROUP": "https://2uwb7d3csg.execute-api.us-east-1.amazonaws.com/LeaveAssignedGroup",
+    "GET_GROUPS": "https://2uwb7d3csg.execute-api.us-east-1.amazonaws.com/GetGroups"
+]
+let DEMO_EMAIL = "berkeley.mobile.demo@gmail.com"
+
+
+
