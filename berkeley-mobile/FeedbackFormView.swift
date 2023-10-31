37d11d0678a63968cd49fadfcc3434bf0d2bfa06
justinwongeecs <jwonghelloworld@berkeley.edu>
2023-10-31 16:40:17 -0700

    add feedback form && change Color to BMColor

---
+++ berkeley-mobile/FeedbackFormView.swift
@@ -0,0 +1,21 @@
+//
+//  FeedbackFormView.swift
+//  berkeley-mobile
+//
+//  Created by Justin Wong on 9/14/23.
+//  Copyright © 2023 ASUC OCTO. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeedbackFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedbackFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedbackFormView()
+    }
+}
