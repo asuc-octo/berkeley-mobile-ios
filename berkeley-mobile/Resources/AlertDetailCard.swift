7bf10b49488678df9518332bdd36895071be5193
adititelang <aditi.telang@berkeley.edu>
2024-04-17 14:47:53 -0700

    expanding text

--- berkeley-mobile/Resources/AlertDetailCard.swift
+++ berkeley-mobile/Resources/AlertDetailCard.swift
@@ -1,16 +1,13 @@
-//
-//  AlertDetailCard.swift
-//  berkeley-mobile
-//
-//  Created by Aditi Telang on 4/12/24.
-//  Copyright © 2024 ASUC OCTO. All rights reserved.
-//
-
+// AlertDetailCard.swift
+// berkeley-mobile
+// Created by Aditi Telang on 4/12/24.
 import SwiftUI
 
 struct AlertDetailCard: View {
     var typeColor: Color
     var notes: String
+    
+    @State private var isDescriptionExpanded: Bool = false
     
     var body: some View {
         ZStack {
@@ -37,39 +34,39 @@ struct AlertDetailCard: View {
                 Text(notes)
                     .font(.caption)
                     .foregroundStyle(.gray)
+                                Button(action: {
+                    self.isDescriptionExpanded.toggle()
+                }) {
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack {
+                            Text(isDescriptionExpanded ? "Detailed description about the robbery incident, including possible suspects, last seen locations, and advised actions for public safety. Tap to see less." : "Description of Incident")
+                                .foregroundColor(.white)
+                                .font(.caption2)
+
+                                .padding([.leading, .trailing], 7)
+                                .animation(.easeInOut, value: isDescriptionExpanded)
+                        }
+                    }
+                    .frame(minHeight: isDescriptionExpanded ? 60 : 28)
+                    .background(Rectangle().fill(Color.gray).cornerRadius(1))
+                }
                 
-                Button(action: {}) {
-                                    ScrollView(.horizontal, showsIndicators: true) {
-                                        HStack {
-                                            Text("Two male subjects wearing dark clothing with ski masks..")
-                                                .foregroundColor(.white)
-                                                .font(.caption2)
-                                                .padding([.leading, .trailing], 7)
-                                        }
-                                    }
-                                    .frame(height: 28)
-                                    .background(Rectangle().fill(Color.blue).cornerRadius(5)) // Background with rounded corners
-                                }
                 decorationsView
             }
             .padding(.horizontal)
             .padding(.vertical, 20)
-            
-            
         }
-       
         .background(Color.white)
         .cornerRadius(10)
         .shadow(radius: 5)
-        .frame(height: 200)
+        .frame(height: isDescriptionExpanded ? 240 : 200)
         .padding()
     }
     
     private var decorationsView: some View {
         HStack {
             Image(systemName: "mappin.and.ellipse")
-            
-            Text("0.5 Distance from you")
+            Text("0.5 mi Distance from you")
                 .font(.footnote)
         }
     }
@@ -78,7 +75,5 @@ struct AlertDetailCard: View {
 struct AlertDetailCard_Previews: PreviewProvider {
     static var previews: some View {
         AlertDetailCard(typeColor: Color(CGColor(gray: 0, alpha: 1)), notes:"West Crescent \n04-12-2024\n06:30:00")
-
     }
 }
-
