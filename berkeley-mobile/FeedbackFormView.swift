f6fc2b803f216c79b9912bcfad66dd575cb82f8e
justinwongeecs <jwonghelloworld@berkeley.edu>
2024-11-12 21:10:34 -0800

    finish feedback form enhancements

--- berkeley-mobile/FeedbackFormView.swift
+++ berkeley-mobile/FeedbackFormView.swift
@@ -6,12 +6,12 @@
 //  Copyright © 2023 ASUC OCTO. All rights reserved.
 //
 
-import SwiftUI
 import Firebase
+import SwiftUI
 
-//Non-Dismissiable FeedbackFormView
+
 struct FeedbackFormView: View {
-    @Environment(\.presentationMode) private var presentationMode
+    @Environment(\.dismiss) private var dismiss
     
     @State private var email = ""
     @State private var selectedMostLikedFeatures = [String]()
@@ -47,76 +47,99 @@ struct FeedbackFormView: View {
             Form {
                 Text("Please take 1-2 minutes to fill out this survey! We really appreciate the feedback! 🙏")
                     .foregroundColor(.secondary)
-                
-                Section(header: Text("Berkeley Email \(!isEmailValid ? "(Required)" : "") ")
-                    .foregroundColor(!isEmailValid ? .red : Color.gray)
-                ) {
-                    TextField("", text: $email)
-                        .padding(10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 5)
-                                .stroke(!isEmailValid ? Color.red : Color.blue, lineWidth: 1)
-                            
-                        )
-                        .background(!isEmailValid ? Color.red.opacity(0.1) : Color.clear)
-                }
-
-                
-                Section(header: Text("What features on the app do you like the most?")) {
-                    ForEach(Array(mostLikedFeaturesOptions.enumerated()), id: \.element) { index, option in
-                        FeedbackFormOptionRow(options: mostLikedFeaturesOptions, selectedOptions: $selectedMostLikedFeatures, optionTitle: option)
-                    }
-                    HStack {
-                        TextField("Other", text: $otherMostLikedFeatures)
-                    }
-                }
-                
-                Section(header: Text("What features do you want on the app?")) {
-                    ForEach(Array(wantedFeaturesOptions.enumerated()), id: \.element) { index, option in
-                        FeedbackFormOptionRow(options: wantedFeaturesOptions, selectedOptions: $selectedWantedFeatures, optionTitle: option)
-                    }
-                    HStack {
-                        TextField("Other", text: $otherWantedFeatures)
-                    }
-                }
-                
-                Section(header: Text("What are you biggest pet peeves with campus?")) {
-                    ForEach(Array(petPeevesOptions.enumerated()), id: \.element) { index, option in
-                        FeedbackFormOptionRow(options: petPeevesOptions, selectedOptions: $selectedPetPeeves, optionTitle: option)
-                    }
-                    HStack {
-                        TextField("Other", text: $otherPetPeeves)
-                    }
-                }
-                
-                Section(header: Text("Any other comments and concerns about the app?")) {
-                    Group {
-                        if #available(iOS 14.0, *) {
-                            TextEditor(text: $otherComments)
-                        } else {
-                            TextField("", text: $otherComments)
-                        }
-                    }
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.blue, lineWidth: 1)
-                    )
-                }
-                
-                Section {
-                    Button(action: {
-                        submitFeedbackForm()
-                    }) {
-                        Text("Submit")
-                            .foregroundColor(.white).bold()
-                            .font(.system(size: 25))
-                    }
-                    .frame(maxWidth: .infinity, minHeight: 40)
-                }
-                .listRowBackground(Color.green.opacity(0.7))
+                emailSection
+                likeFeaturesSection
+                wantFeaturesSection
+                petPeevesSection
+                otherCommentsSection
+                submitButtonSection
             }
             .navigationBarTitle("We Want Your Feedback!", displayMode: .inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        dismissForm()
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 20))
+                            .foregroundStyle(.gray)
+                    }
+                }
+            }
         }
+    }
+    
+    private var emailSection: some View {
+        Section(header: Text("Berkeley Email \(!isEmailValid ? "(Required)" : "") ")
+            .foregroundColor(!isEmailValid ? .red : Color.gray)
+        ) {
+            TextField("", text: $email)
+                .padding(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(!isEmailValid ? Color.red : Color.blue, lineWidth: 1)
+                    
+                )
+                .background(!isEmailValid ? Color.red.opacity(0.1) : Color.clear)
+        }
+    }
+    
+    private var likeFeaturesSection: some View {
+        Section(header: Text("What features on the app do you like the most?")) {
+            ForEach(Array(mostLikedFeaturesOptions.enumerated()), id: \.element) { index, option in
+                FeedbackFormOptionRow(options: mostLikedFeaturesOptions, selectedOptions: $selectedMostLikedFeatures, optionTitle: option)
+            }
+            HStack {
+                TextField("Other", text: $otherMostLikedFeatures)
+            }
+        }
+    }
+    
+    private var wantFeaturesSection: some View {
+        Section(header: Text("What features do you want on the app?")) {
+            ForEach(Array(wantedFeaturesOptions.enumerated()), id: \.element) { index, option in
+                FeedbackFormOptionRow(options: wantedFeaturesOptions, selectedOptions: $selectedWantedFeatures, optionTitle: option)
+            }
+            HStack {
+                TextField("Other", text: $otherWantedFeatures)
+            }
+        }
+    }
+    
+    private var petPeevesSection: some View {
+        Section(header: Text("What are you biggest pet peeves with campus?")) {
+            ForEach(Array(petPeevesOptions.enumerated()), id: \.element) { index, option in
+                FeedbackFormOptionRow(options: petPeevesOptions, selectedOptions: $selectedPetPeeves, optionTitle: option)
+            }
+            HStack {
+                TextField("Other", text: $otherPetPeeves)
+            }
+        }
+    }
+    
+    private var otherCommentsSection: some View {
+        Section(header: Text("Any other comments and concerns about the app?")) {
+            TextEditor(text: $otherComments)
+        }
+    }
+    
+    private var submitButtonSection: some View {
+        Section {
+            Button(action: {
+                submitFeedbackForm()
+            }) {
+                HStack {
+                    Spacer()
+                    Text("Submit")
+                        .foregroundColor(.white).bold()
+                        .font(.system(size: 25))
+                    Spacer()
+                }
+                .padding()
+            }
+            .buttonStyle(DepthButtonStyle(color: Color.green))
+        }
+        .listRowBackground(Color.clear)
     }
     
     private func submitFeedbackForm() {
@@ -139,8 +162,12 @@ struct FeedbackFormView: View {
                 otherComments
         ])
         
-        presentationMode.wrappedValue.dismiss()
-        UserDefaults.standard.set(true, forKey: UserDefaultKeys.hasShownFeedbackFrom)
+        dismissForm()
+    }
+    
+    private func dismissForm() {
+        dismiss()
+        UserDefaults.standard.set(2, forKey: UserDefaultKeys.numAppLaunchForFeedbackForm)
     }
 }
 
@@ -150,7 +177,9 @@ struct FeedbackFormView_Previews: PreviewProvider {
     }
 }
 
+
 //MARK: - FeedbackFormOptionRow
+
 struct FeedbackFormOptionRow: View {
     var options: [String]
     @Binding var selectedOptions: [String]
