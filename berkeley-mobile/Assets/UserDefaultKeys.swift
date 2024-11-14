df38467f1c09d0180b74c63622f886950faa9694
Justin W. <51015074+justinwongeecs@users.noreply.github.com>
2024-11-13 23:32:44 -0800

    finish feedback form enhancements (#316)

--- berkeley-mobile/Assets/UserDefaultKeys.swift
+++ berkeley-mobile/Assets/UserDefaultKeys.swift
@@ -13,5 +13,5 @@ struct UserDefaultKeys {
         static let hasShownNewFeature = "hasShownNewFeature"
         static let hasShownOnboarding = "hasShownOnboarding"
     }
-    static let hasShownFeedbackFrom = "hasShownFeedbackForm"
+    static let numAppLaunchForFeedbackForm = "numAppLaunchForFeedbackForm"
 }
