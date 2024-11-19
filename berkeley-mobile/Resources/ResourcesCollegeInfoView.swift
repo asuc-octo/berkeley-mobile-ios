796d46d4d5fdc9a3464fd46b5955542e930ea129
sahanabhar <sahanabharadwaj@berkeley.edu>
2024-11-18 16:53:40 -0800

    finished college info view

---
+++ berkeley-mobile/Resources/ResourcesCollegeInfoView.swift
@@ -0,0 +1,19 @@
+//
+//  ResourcesCollegeInfoView.swift
+//  berkeley-mobile
+//
+//  Created by Sahana Bharadwaj on 10/24/24.
+//  Copyright © 2024 ASUC OCTO. All rights reserved.
+//
+
+import SwiftUI
+
+struct ResourcesCollegeInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResourcesCollegeInfoView()
+}
