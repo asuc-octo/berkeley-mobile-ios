2e70789ce316b82ab3168cf9530f8701c2c26d27
Iyu <138597523+iychi@users.noreply.github.com>
2025-02-28 15:36:34 -0800

    BER-50: Update MapMarkersDropdownView to BMFont (#327)
    * MapMarkersDropdownView Font update
    
    * change font size to 20

--- berkeley-mobile/Map/MapMarkersDropdownView.swift
+++ berkeley-mobile/Map/MapMarkersDropdownView.swift
@@ -65,10 +65,7 @@ struct MapMarkersDropdownView: View {
                     HStack(spacing: 15) {
                         Image(uiImage: type.icon())
                         Text(type.rawValue)
-                            .fontWeight(.semibold)
-                            .font(.system(size: 19))
-                            .foregroundStyle(.primary)
-                        Spacer()
+                            .font(Font(BMFont.regular(20)))
                     }
                     .contentShape(Rectangle())
                 }
