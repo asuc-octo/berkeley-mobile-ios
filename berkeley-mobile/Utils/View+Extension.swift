5ae181c6faaf421dc69ef860853d693b87fa211c
Iyu <138597523+iychi@users.noreply.github.com>
2025-03-15 01:26:16 -0700

    BER-72: Rewrite CardView in SwiftUI as a View Modifier (#340)
    add cardify view modifier in View+Extension.swift.

--- berkeley-mobile/Utils/View+Extension.swift
+++ berkeley-mobile/Utils/View+Extension.swift
@@ -25,3 +25,22 @@ struct HomeMapControlButtonStyle: ButtonStyle {
             )
     }
 }
+
+
+struct Cardify: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(3)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color(BMColor.cardBackground)) 
+            )
+            .shadow(color: Color.black.opacity(0.25), radius: 5, x: 0, y: 0)
+    }
+}
+
+extension View {
+    func cardify() -> some View {
+        self.modifier(Cardify())
+    }
+}
