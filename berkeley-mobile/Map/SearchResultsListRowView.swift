b7794bcd7f81f9a24e7700e1dcb8f74c14ea5e21
Baurzhan Abenov <89326250+baeuke@users.noreply.github.com>
2025-03-29 19:13:16 -0700

    BER-70: Rewrite Home Search View (#347)
    * ber-70: searchBar rewrite to SwiftUI
    
    * minor clean up
    
    * CLLocation extension: distanceFromUser()
    
    * updated hasLocation to use distanceFromUser()
    
    * Refactored SearchResultsView - no more mask view

---
+++ berkeley-mobile/Map/SearchResultsListRowView.swift
@@ -0,0 +1,69 @@
+//
+//  SearchResultsListRowView.swift
+//  berkeley-mobile
+//
+//  Created by Baurzhan on 3/19/25.
+//  Copyright © 2025 ASUC OCTO. All rights reserved.
+//
+
+import MapKit
+import SwiftUI
+
+struct SearchResultsListRowView: View {
+    let placemark: MapPlacemark
+    
+    var distance: Double? {
+        guard let placemarkLocation = placemark.location else {
+            return nil
+        }
+        
+        return placemarkLocation.distanceFromUser()
+    }
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .center) {
+                Image(systemName: "mappin")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 8)
+                    .foregroundStyle(Color(BMColor.searchBarIconColor))
+                
+                if let distance {
+                    Text("\(distance, specifier: "%.1f") mi")
+                        .font(Font(BMFont.regular(12)))
+                }
+            }
+            .frame(width: 52)
+            
+            VStack(alignment: .leading) {
+                Text(placemark.searchName ?? "Not Available")
+                    .font(Font(BMFont.regular(16)))
+                
+                Text(placemark.locationName ?? "Not Available")
+                    .font(Font(BMFont.regular(14)))
+            }
+            .frame(height: 36)
+            
+            Spacer()
+        }
+        .padding(10)
+        .frame(height: 60.0)
+    }
+}
+
+#Preview {
+    let previewCoordinates = CLLocation(latitude: 37.87538, longitude: -122.25612109999999)
+    let previewPlacemark = MapPlacemark(loc: previewCoordinates, name: "Foothill", locName: "2700 Hearst Ave, Berkeley, CA 94720")
+    
+    VStack {
+        Button(action: {}) {
+            SearchResultsListRowView(placemark: previewPlacemark)
+        }
+        .buttonStyle(SearchResultsListRowButtonStyle())
+    }
+    .background(.regularMaterial)
+    .clipShape(.rect(cornerRadius: 12))
+    .padding()
+}
+
---
+++ berkeley-mobile/Map/SearchViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  SearchViewModel.swift
+//  berkeley-mobile
+//
+//  Created by Baurzhan on 3/19/25.
+//  Copyright © 2025 ASUC OCTO. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+// MARK: - SearchResultsViewDelegate protocol
+
+protocol SearchResultsViewDelegate {
+    func choosePlacemark(_ placemark: MapPlacemark)
+    func chooseMapMarker(_ placemark: MapMarker)
+}
+
+
+// MARK: - SearchResultsState
+
+enum SearchResultsState {
+    case idle // When SearchBar text field is empty (includes inital state)
+    case loading
+    case populated([MapPlacemark])
+    case empty
+    case error(Error)
+    
+    var placemarks: [MapPlacemark] {
+        switch self {
+        case .populated(let placemarks):
+            return placemarks
+        default:
+            return []
+        }
+    }
+}
+
+
+// MARK: - SearchViewModel
+
+class SearchViewModel: ObservableObject {
+    @Published var searchText = ""
+    @Published var isSearching = false
+    @Published var state = SearchResultsState.idle
+    var chooseMapMarker: (MapMarker) -> Void
+    var choosePlacemark: (MapPlacemark) -> Void
+    
+    init(chooseMapMarker: @escaping (MapMarker) -> Void, choosePlacemark: @escaping (MapPlacemark) -> Void) {
+        self.chooseMapMarker = chooseMapMarker
+        self.choosePlacemark = choosePlacemark
+    }
+    
+    
+    // MARK: - SearchBarView's funcs:
+    
+    func clearSearchText() {
+        searchText = ""
+    }
+    
+    func searchLocations(_ keyword: String, completion: (([MapPlacemark], Error?) -> Void)? = nil) {
+        state = .loading
+        
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            guard let self else {
+                return
+            }
+            
+            let data = DataManager.shared.searchable
+            let filtered = data.filter { ($0.searchName.lowercased().contains(keyword.lowercased()) && $0.location.0 != 0 && $0.location.1 != 0) }
+            var placemarks = [MapPlacemark]()
+            
+            for item in filtered {
+                let cl = CLLocation(latitude: CLLocationDegrees(item.location.0), longitude: CLLocationDegrees(item.location.1))
+                let place = MapPlacemark(loc: cl, name: item.searchName, locName: item.locationName, item: item)
+                placemarks.append(place)
+            }
+            DispatchQueue.main.async {
+                self.updatePlacemarksList(newPlacemarks: placemarks, error: nil)
+                completion?(placemarks, nil)
+            }
+        }
+    }
+    
+    
+    // MARK: - SearchResultsView's funcs:
+    
+    func updatePlacemarksList(newPlacemarks: [MapPlacemark]?, error: Error?) {
+        if let error {
+            state = .error(error)
+            return
+        }
+        
+        guard let newPlacemarks, !newPlacemarks.isEmpty else {
+            if searchText.isEmpty {
+                state = .idle
+            } else {
+                state = .empty
+            }
+            return
+        }
+        
+        state = .populated(newPlacemarks)
+    }
+    
+    func selectListRow(_ placemark: MapPlacemark) {
+        searchText = ""
+        isSearching = false
+        
+        if let mapMarker = placemark.item as? MapMarker {
+            chooseMapMarker(mapMarker)
+        }
+        else {
+            choosePlacemark(placemark)
+        }
+    }
+}
