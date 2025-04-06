e29742c29731489d76477fb6f3ae4f4773219bd7
hetvi15986 <141077758+hetvi15986@users.noreply.github.com>
2025-04-06 14:34:19 -0700

    BER-68: Create BMFilterButton in SwiftUI (#354)
    * added BMFilterButton.swift under Content/FilterView; unclean code; previews not hidden
    
    * Formatting
    
    * Change Header Name
    
    * Delete berkeley-mobile/Common/FilterView/BMFilterButton.swift
    
    * Change buttonStyle, update formatting

---
+++ berkeley-mobile/Common/BMFilterButton.swift
@@ -0,0 +1,47 @@
+//
+//  BMFilterButton.swift
+//  berkeley-mobile
+//
+//  Created by Hetvi Patel on 4/1/25.
+//  Copyright © 2025 ASUC OCTO. All rights reserved.
+//
+
+import SwiftUI
+
+struct BMFilterButton: View {
+    @Binding var isSelected: Bool
+    let title: String
+    
+    var body: some View {
+        Button(action: {
+            isSelected.toggle()
+        }) {
+            Text(title)
+                .font(.system(size: 14, weight: .bold))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 5)
+                .background(isSelected ? Color(uiColor: BMColor.selectedButtonBackground) : Color(uiColor: BMColor.cardBackground))
+                .foregroundColor(isSelected ? .white : Color(uiColor: BMColor.secondaryText))
+                .cornerRadius(14)
+                .shadow(color: .black.opacity(0.25), radius: 5, x: 0, y: 0)
+                .frame(height: 28)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct BMFilterButtonPreviewView: View {
+    @State private var nearbySelected = false
+
+    var body: some View {
+        HStack {
+            BMFilterButton(isSelected: $nearbySelected, title: "Nearby")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BMFilterButtonPreviewView()
+}
