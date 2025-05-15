aeab9b24c775b76585d4c744d83665602a2a056d
Justin W. <51015074+justinwongeecs@users.noreply.github.com>
2025-05-15 00:46:20 -0700

    BER-108: Prevent Readding Events To Calendar (#373)
    * finish
    
    * use EKError code name instead of raw value
    
    * improvements
    
    * improvements & bug fixes
    
    * update BMError descriptions
    
    * update build number

--- berkeley-mobile/Data/EventManager.swift
+++ berkeley-mobile/Data/EventManager.swift
@@ -15,26 +15,49 @@ class EventManager {
     private let eventStore = EKEventStore()
     
     public func addEventToCalendar(calendarEvent: CalendarEvent) async throws {
-        var success = false
-        
         if #available(iOS 17.0, *) {
-            success = try await eventStore.requestWriteOnlyAccessToEvents()
+            try await eventStore.requestFullAccessToEvents()
         } else {
-            success = try await eventStore.requestAccess(to: .event)
+            try await eventStore.requestAccess(to: .event)
         }
         
-        try saveEvent(calendarEvent, shouldSave: success)
+        try saveEvent(calendarEvent)
     }
     
-    private func saveEvent(_ calendarEvent: CalendarEvent, shouldSave: Bool) throws {
+    private func saveEvent(_ calendarEvent: CalendarEvent) throws {
+        let eventStartDate = calendarEvent.date
+        let eventEndDate = calendarEvent.end ?? calendarEvent.date
+        
+        let predicate = eventStore.predicateForEvents(withStart: eventStartDate, end: eventEndDate, calendars: nil)
+        let existingEvents = eventStore.events(matching: predicate)
+        let eventAlreadyExists = existingEvents.contains { $0.title == calendarEvent.name && $0.startDate == eventStartDate && $0.endDate == eventEndDate }
+        
+        guard !eventAlreadyExists else {
+            throw BMError.eventAlreadyAddedInCalendar
+        }
+        
         let event: EKEvent = EKEvent(eventStore: eventStore)
         event.title = calendarEvent.name
-        event.startDate = calendarEvent.date
-        event.endDate = calendarEvent.end ?? calendarEvent.date
+        event.startDate = eventStartDate
+        event.endDate = eventEndDate
         event.location = calendarEvent.location
         event.notes = calendarEvent.additionalDescription + (calendarEvent.descriptionText ?? "")
         event.calendar = eventStore.defaultCalendarForNewEvents
         
-        try eventStore.save(event, span: .thisEvent)
+        do {
+            try eventStore.save(event, span: .thisEvent)
+            if #available(iOS 17.0, *) {
+                let authStatus = EKEventStore.authorizationStatus(for: .event)
+                if authStatus == .writeOnly {
+                    throw BMError.mayExistedInCalendarAlready
+                }
+            }
+        } catch {
+            if let ekError = error as? EKError, ekError.errorCode == 1 {
+                throw BMError.insufficientAccessToCalendar
+            } else {
+                throw error
+            }
+        }
     }
 }
