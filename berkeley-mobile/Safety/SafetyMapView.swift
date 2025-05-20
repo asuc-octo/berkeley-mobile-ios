57a5fd0a9c0828c0d352ca33dfdaff4e6118a2da
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-05-19 21:27:35 -0700

    finish

--- berkeley-mobile/Safety/SafetyMapView.swift
+++ berkeley-mobile/Safety/SafetyMapView.swift
@@ -51,6 +51,7 @@ struct SafetyNewMapView: View {
     var body: some View {
         ZStack {
             Map(position: $mapCameraPosition, bounds: bounds, selection: $selectedSafetyLog) {
+                UserAnnotation()
                 ForEach(safetyViewModel.filteredSafetyLogs) { safetyLog in
                     let dateText = "\(safetyLog.date.formatted(date: .numeric, time: .shortened))"
                     let monogramText = "\(safetyLog.getSafetyLogState.rawValue.first!.uppercased())"
