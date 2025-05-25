b114e15f0c920d7d4a4f4ab4816fcf596425ef17
justinwongeecs <jwonghelloworld@berkeley.edu>
2025-05-25 13:36:02 -0700

    use BMEventCalendarEntry.sampleEvent

--- berkeley-mobile/Events/EventDataSource/BMEventCalendarEntry.swift
+++ berkeley-mobile/Events/EventDataSource/BMEventCalendarEntry.swift
@@ -130,7 +130,7 @@ extension BMEventCalendarEntry {
         date: Date(),
         end: Date().addingTimeInterval(7200),
         descriptionText: "Mention of the name University of California, Berkeley, evokes a range of images: a celebrated institution, a seat of innovation, protests and activism, iconic architecture, colorful traditions, and … literary muse? The campus has long sparked the creativity of fiction writers, inspiring them to use it as a backdrop, a key player, or a barely disguised character within their tales. This exhibition highlights examples of these portrayals through book covers, excerpts, illustrations, photographs, and other materials largely selected from the University Archives and general collections of The Bancroft Library.",
-        location: "The Rowell Exhibition Cases, Doe Library, 2nd floo...",
+        location: "The Rowell Exhibition Cases, Doe Library, 2nd floor",
         registerLink: "https://berkeley.edu/register",
         imageURL: "https://events.berkeley.edu/live/image/gid/139/width/200/height/200/crop/1/src_region/0,0,3200,2420/4595_cubanc00006587_ae_a.rev.1698182194.jpg",
         sourceLink: "https://berkeley.edu/event",
